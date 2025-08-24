ada8cad323518861427b51411ea3870c09001c93
Caceresenzo <caceresenzo1502@gmail.com>
2025-08-24 03:39:50 +0200

    CLI tests (cli-test-1755999590775)

--- src/main/java/grep/Main.java
+++ src/main/java/grep/Main.java
@@ -1,16 +1,54 @@
 package grep;
 
+import java.io.IOException;
 import java.util.Scanner;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.help.HelpFormatter;
+
+import lombok.SneakyThrows;
 
 public class Main {
 
 	public static void main(String[] args) {
-		if (args.length != 2 || !args[0].equals("-E")) {
-			System.out.println("Usage: ./your_program.sh -E <pattern>");
-			System.exit(1);
+		final var helpOption = new Option(null, "help", false, "display this help text and exit");
+
+		final var extendedRegexpOption = new Option("E", "extended-regexp", false, "PATTERNS are extended regular expressions");
+		extendedRegexpOption.setRequired(true);
+
+		final var options = new Options();
+		options.addOption(helpOption);
+		options.addOption(extendedRegexpOption);
+
+		final CommandLine commandLine;
+
+		try {
+			commandLine = new DefaultParser().parse(options, args);
+		} catch (ParseException exception) {
+			System.err.println(exception.getMessage());
+			throw printUsage(options);
 		}
 
-		String patternString = args[1];
+		if (commandLine.hasOption(helpOption)) {
+			throw printUsage(options);
+		}
+
+		final var argList = commandLine.getArgList();
+		if (argList.isEmpty()) {
+			System.err.println("A PATTERN is required.");
+			throw printUsage(options);
+		}
+
+		if (!commandLine.hasOption("E")) {
+			System.err.println("The -E option is required.");
+			throw printUsage(options);
+		}
+
+		final var patternString = commandLine.getArgList().get(0);
 
 		try (final var scanner = new Scanner(System.in)) {
 			String inputLine = scanner.nextLine();
@@ -27,6 +65,18 @@ public class Main {
 			exception.printStackTrace();
 			System.exit(2);
 		}
+	}
+
+	@SneakyThrows
+	static RuntimeException printUsage(Options options) {
+		final var helpFormatter = HelpFormatter.builder()
+			.setShowSince(false)
+			.get();
+
+		helpFormatter.printHelp("grep [OPTION]... PATTERNS [FILE]...", "Search for PATTERNS in each FILE.", options, null, false);
+		System.exit(1);
+
+		return null;
 	}
 
 }
